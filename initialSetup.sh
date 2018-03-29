e4c7baabb4dbc4d5e52ec252ae97cfe2400ab268
jreichmann <34141868+jreichmann@users.noreply.github.com>
2018-03-29 15:07:40 +0200

    Link .tmux.conf to tmux configuration from repo
    The executing users ~/.tmux.conf will be symlinked to the repos tmux configuration file

--- initialSetup.sh
+++ initialSetup.sh
@@ -5,6 +5,9 @@ sudo -i dnf install tmux tree git
 
 # Checkout my dotfiles and the tmux-plugin-manager
 mkdir -p ~/.tmux/plugins
+mkdir -p ~/.dotfiles
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
-git clone https://github.com/jreichmann/dotfiles.git
+git clone https://github.com/jreichmann/dotfiles.git ~/.dotfiles
+
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
