85a2f164a935f9025b568fe1b351fa51ddd243f3
Johanna Reichmann <jreichmann@finallycoffee.eu>
2018-05-11 23:10:44 +0200

    Add XFCE4 Keybindings to dotfiles
    The Keybindings are located in the XML File, and the script creates a symlink to them

---
+++ keybindings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+ln -s ~/.dotfiles/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
